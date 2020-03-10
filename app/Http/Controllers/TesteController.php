36c5b583be77001aa22fd0c9ae8d7d0b5759673b
OlivioJunio <39318841+OlivioJunio@users.noreply.github.com>
2020-03-09 21:46:15 -0300

    Commit primeira aula

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function index(){
+        //return 'Noxus';
+        //return view('Layouts/LayoutFull');
+        
+    }
+}
